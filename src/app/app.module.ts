5f72b8d0b71bfc90f6b7e08161083cbd5cb50c89
Feroz Ahmmed <froghramar@gmail.com>
2018-09-04 21:34:07 +0600

    update for server side rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Inject, PLATFORM_ID, APP_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({
+      appId: 'angular-server-side-rendering'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+  @Inject(APP_ID) private appId: string){
+    const platform = isPlatformBrowser(platformId) ? 'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
